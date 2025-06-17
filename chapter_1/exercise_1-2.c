ea4609b29842cc6b40f1c9886fbdb90892214de0
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-06-17 22:30:40 +0800

    introduction

---
+++ chapter_1/exercise_1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("tab is printed between A and B\n");
+  printf("A\tB\n");
+
+  printf("backslash is printed between C and D\n");
+  printf("C\\D\n");
+
+  return 0;
+}
