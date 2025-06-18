6edb84f1c7cc25735d37adfe305f78384823bf9b
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-06-18 20:48:26 +0800

    exercise 1-5

--- chapter_1/exercise_1-4.c
+++ chapter_1/exercise_1-4.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	float celsius, fahr;
-	float lower, upper, step;
+  float celsius, fahr;
+  float lower, upper, step;
 
-	lower = 0;
-	upper = 100;
-	step = 8;
+  lower = 0;
+  upper = 100;
+  step = 8;
 
-	printf("Celsius\tFahr\n");
+  printf("Celsius\tFahr\n");
 
-	celsius = lower;
-	while (celsius <= upper) {
-		fahr = celsius * 9.0 / 5.0 + 32.0;
-		printf("%7.0f\t%3.1f\n", celsius, fahr);
+  celsius = lower;
+  while (celsius <= upper) {
+    fahr = celsius * 9.0 / 5.0 + 32.0;
+    printf("%7.0f\t%3.1f\n", celsius, fahr);
 
-		celsius = celsius + step;
-	}
+    celsius = celsius + step;
+  }
 
-	return 0;
+  return 0;
 }
---
+++ chapter_1/exercise_1-5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	printf("Fahr\tCelsius\n");
+	for (float fahr = 300; fahr >= 0; fahr = fahr - 20) {
+		printf("%4.0f\t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
+	}
+
+	return 0;
+}
