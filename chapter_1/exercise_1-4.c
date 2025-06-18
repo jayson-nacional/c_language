cfaa7e5aee8378190afdeec7df9c6c86fab3cf8d
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-06-18 20:38:51 +0800

    exercise 1-4

---
+++ chapter_1/exercise_1-4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	float celsius, fahr;
+	float lower, upper, step;
+
+	lower = 0;
+	upper = 100;
+	step = 8;
+
+	printf("Celsius\tFahr\n");
+
+	celsius = lower;
+	while (celsius <= upper) {
+		fahr = celsius * 9.0 / 5.0 + 32.0;
+		printf("%7.0f\t%3.1f\n", celsius, fahr);
+
+		celsius = celsius + step;
+	}
+
+	return 0;
+}
