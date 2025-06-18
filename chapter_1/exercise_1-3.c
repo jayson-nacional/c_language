319a18121f1ebcd6b7263be4780f6e30d9f254d7
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-06-18 21:19:02 +0800

    formatting adjustments

--- chapter_1/exercise_1-3.c
+++ chapter_1/exercise_1-3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	float fahr, celsius;
-	float lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
+  lower = 0;
+  upper = 300;
+  step = 20;
 
-	printf("Fahr\tCelsius\n");
-	fahr = lower;
-	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f %11.1f\n", fahr, celsius);
-		fahr = fahr + step;
-	}
+  printf("Fahr\tCelsius\n");
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %11.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
 
-	return 0;
+  return 0;
 }
