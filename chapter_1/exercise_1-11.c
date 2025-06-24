e66de62294db5dd3ef335f46a64ed46339e83064
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-06-24 16:13:08 +0800

    exercise 1-11

---
+++ chapter_1/exercise_1-11.c
@@ -0,0 +1,15 @@
+/*
+* How would you test the word count program?
+* What kind of input are most likely to uncover bugs if there are any?
+*
+* Answer: Potentially test with combination of trailing blanks.
+* Series of words separated by comma may be incorrectly interpreted by the program
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Exercise 1-11");
+
+	return 0;
+}
