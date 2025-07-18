7915655e457e2ca84bc9d9d33579a853acbace9a
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-07-18 08:46:40 +0800

    final exercise 2-7

--- chapter_2/exercise_2-7.c
+++ chapter_2/exercise_2-7.c
@@ -3,20 +3,6 @@
 unsigned invert(unsigned x, int p, int n);
 
 int main() {
-  printf("Testing program...\n");
-  // Write a function invert (x, p, n) that returns x
-  // with the n bits that begin at position p inverted (i.e., 1 changed to 0 and
-  // vice versa), leaving others unchanged
-  //
-  // i.e. invert(22, 4, 3)
-  // 1 0 1 1 0
-  // 0 1 0 1 0
-  // answer is 12
-
-  // thought process
-  // create invert of 0 ~0 => 1 1 1 1 1 1 1 1 1
-  // 1 1 1 1 1 0 0 0 0
-  // 0 0 0 1 1 1 0 0
   printf("%u\n", invert(22, 4, 3));
 
   return 0;
